2bae22a17d958e4e24e02a40a4dc9dc7021b76be
hmvtonning <114178076+hmvtonning@users.noreply.github.com>
2024-02-29 15:13:51 +0100

    Master problem implementation (#9)
    * started implementation of L-shaped master problem
    
    * added data_class
    
    * L-shaped master problem running, dataclasses defined and algorithm started
    
    ---------
    
    Co-authored-by: Henrik Martin Vassbotten <hmtonnin@ntnu.no>

--- data_classes.py
+++ data_classes.py
@@ -8,3 +8,13 @@ class LShapedMasterProblemVariables():
     deploy_type_bin: list[list[int]] # index order f, t. same unique l to be added in all subproblems from a master problem
     
 
+@dataclass
+class LShapedSubProblemDualVariables(): 
+    rho_1: list[float] # index order t. Also needs s as scenario index
+    rho_2: list[list[float]] # index order f, t. Also needs s as scenario index
+    rho_3: list[float] # index order t. Also needs s as scenario index
+    rho_4: float # Only needs scenario index s 
+    rho_5: list[list[float]] # index order t_hat, t. Also needs s as scenario index
+    rho_6: list[float] # index order t. Also needs s as scenario index
+    rho_7: list[float] # index order t. Also needs s as scenario index
+
--- l_shaped_algorithm.py
+++ l_shaped_algorithm.py
@@ -1,14 +1,23 @@
+from l_shaped_master_problem import LShapedMasterProblem
+import initialization.sites as sites 
 
 class LShapedAlgoritm:
     def __init__(self) -> None:
         pass
-'''
-    : Init and solve master problem w/o optimality cut
-    : Init subproblems based on master problem solution
-        : Solve SPs
-        : Add optimality cut to MP
-        : Solve master problem
-        : Pass MP values to SPs
-        : Return to step 3 if MP solution != previous MP solution
-        : Terminate
-'''
+
+
+    def run():
+        master_problem = LShapedMasterProblem(sites.short_sites_list, 0) # TODO: change input
+        master_problem.initialize_model()
+        master_problem.solve()
+        old_master_problem_solution = None
+        new_master_problem_solution = master_problem.get_variable_values()
+        while new_master_problem_solution != old_master_problem_solution:
+            old_master_problem_solution = new_master_problem_solution # Might need deepcopy here (?)
+            # TODO: pass new_master_problem_solution to subproblems
+            # TODO: solve subproblems and store dualvariables in a list of LShapedSubProblemDualVariable-objects, with dimension 1*s
+            master_problem.add_optimality_cuts(dual_variables)
+            master_problem.solve()
+            new_master_problem_solution = master_problem.get_variable_values()
+       #TODO: decide what to return. Will change when we integrate into DW-decomp
+
